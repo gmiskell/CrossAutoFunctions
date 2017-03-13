a3e7f377e5d1a90d7dc9c46e3219d47e5f01aae0
Georgia Miskell <georgia.miskell@gmail.com>
2017-03-13 15:36:40 +1300

    Updates
    Definition of each function in order to deliver similar outputs

--- R/auto_cross.r
+++ R/auto_cross.r
@@ -1,59 +1,80 @@
-  # clause on either auto or cross test to be run
-  if (auto == TRUE){
-  cast.dat <- x %>%
-    filter(date >= as.POSIXct(date.start) - days(ell), date < (date.end)) %>%
-	mutate(day = str_sub(date, end = 10)) %>%
-    group_by(site) %>%
-	filter(!is.na(pol)) 	
-	len <- as.numeric(difftime(cast.dat$date[2],cast.dat$date[1],units = 'mins'))
+#' A function to set up data ready to perform "auto" analysis
+#'
+#' This function reformats the data relative to auto analysis, where site is held steady.
+#' @param x This is the column under observation.
+#' @param id This is the id of each set of data being compared.
+#' @param group1 This sets the length of time backwards (in days) that should be considered as the auto proxy. Defaults to 3.
+#' @param group2 This is the column which identifies separate networks within the data.
+#' @param date.start This sets the start of the selected data. Defaults to '2016-07-01'.
+#' @param date.end This sets the end of the selected data. Defaults to one week later than `date.start`.
+#` @export
+#' @examples
+#' autoFUN()
+
+autoFUN <- function(x, id, group1 = 3, group2, date.start = '2016-07-01', date.end = NA){
+  
+  list.of.packages <- c("plyr", "raster", "stringr", "data.table", "lubridate", "dplyr")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+ 
+  library(plyr); library(raster); library(data.table); library(stringr); library(lubridate); library(dplyr)
+  
+  date.start.ell <- as.character(as.Date(date.start) - days(group1))
+  if(is.na(date.end) == TRUE) {date.end <- as.character(as.Date(date.start) + days(7))}
+  
+  x <- x %>%
+	select(date, id, group2, value) %>%
+    filter(date >= (date.start.ell), date < (date.end)) %>%
+    group_by(id) %>%
+	filter(!is.na(group2)) %>%
+	arrange(date)
+	len <- as.numeric(difftime(x$date[2], x$date[1], units = 'mins'))
 	if(len == 60) len = 24
 	if(len == 1) len = 1440	
-	nn <- seq(1:ell) * len		
-	cast.dat <- setDT(all.data.melt.hr)[, paste("l", 1:ell) := shift(value, n =  nn),by = site]
-	cast.dat <- data.frame(cast.dat)
-  } else {
-  # rearrange data structure and create day variable
-  cast.dat <- x %>%  
-    filter(date >= (date.start) & date < (date.end)) %>%
-    dcast(... ~ site, value.var = 'value', median) %>%
-    mutate(day = str_sub(date, end = 10L))
-  }
-  
-  
-  # sort data into auto function ----
+	nn <- seq(1:group1) * len		
+	x <- setDT(x)[, paste("l", 1:group1) := shift(value, n =  nn, type = 'lead'), by = id]
+	x <- data.frame(x)
+	
+  y <- x %>%
+	mutate(response.value = value) %>%
+	group_by(date, id) %>%
+	melt(id.vars = c('date', 'id', 'group2', 'response.value')) %>%
+	rename(comparison = variable, comparison.value = value)
 
-autoFN <- function(x, ell = 3, t = '2016-12-01'){
-  
-  x$day <- str_sub(x$date, end = 10)
-
-  response <- x %>%
-    filter(day == t) %>%
-    mutate(time = str_sub(date, start = 12)) %>%
-    select(time, site, resp = value)
-  
-  comparison <- x %>%
-    filter(as.Date(day) >= as.Date(t)-ell & as.Date(day) <= as.Date(t)-1) %>%
-    mutate(time = str_sub(date, start = 12)) %>%
-    select(time, site, comp = value)
-  
-  z <- merge(response, comparison, by = 'time')
-  
 }
 
-crossFN <- function(x, site2 = 'B1', t = '2016-12-01'){
+#' A function to set up data ready to perform "cross" analysis
+#'
+#' This function reformats the data relative to cross analysis, where time is held steady. NB: id & group1 here are the same, and so only id is included.
+#' @param x This is the column under observation.
+#' @param id This is the id of each set of data being compared.
+#' @param group2 This is the column which identifies separate networks within the data.
+#' @param date.start This sets the start of the selected data. Defaults to '2016-07-01'.
+#' @param date.end This sets the end of the selected data. Defaults to one week later than `date.start`.
+#` @export
+#' @examples
+#' crossFUN()
+
+crossFN <- function(x, id, group2, date.start = '2016-07-01', date.end = NA){
   
-  x$day <- str_sub(x$date, end = 10)
+  list.of.packages <- c("reshape2", "lubridate", "dplyr")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+ 
+  library(reshape2); library(lubridate); library(dplyr)
   
-  response <- x %>%
-    filter(day == t) %>%
-    mutate(time = str_sub(date, start = 12)) %>%
-    select(time, resp.site = site, resp = value)
+  if(is.na(date.end) == TRUE) {date.end <- as.character(as.Date(date.start) + days(7))}
   
-  comparison <- x %>%
-    filter(day == t & site != site2) %>%
-    mutate(time = str_sub(date, start = 12)) %>%
-    select(time, comp.site = site, comp = value)
-  
-  z <- merge(response, comparison, by = 'time')
+  x <- x %>%
+    select(date, id, value, group2) %>%
+	filter(date >= (date.start) & date <= (date.end))
+	
+  y <- x %>%
+	mutate(response.value = value) %>%
+	group_by(date, id) %>%
+	dcast(... ~ id + group2) %>%
+	melt(id.vars = c('date', 'id', 'group2', 'response.value') %>%
+	rename(comparison = variable, comparison.value = value)
+	
 
 }
