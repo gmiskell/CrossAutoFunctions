333b3fcc726602ec0b64878f98d5d34e30ba53c1
Georgia Miskell <georgia.miskell@gmail.com>
2018-01-18 14:26:42 +1300

    Update ksFUN.R

--- R/ksFUN.R
+++ R/ksFUN.R
@@ -15,78 +15,82 @@
 #' ksFUN()
 
 
-ksFUN <- function(x, obs, date, proxy, reflective = TRUE, date.start = '2016-07-01 00:00:00', date.end = '2016-07-10 00:00:00', theta = NA, tau = NA, window.length = 72){
+ksFUN <- function(x, obs, date, proxy, reflective = TRUE, theta = NA, tau = NA, window.length = 72){
   
   # install and load required packages
-    list.of.packages <- c("raster", "ggplot2", "plyr", "data.table", "stringr", "dplyr")
-    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-    if(length(new.packages)) install.packages(new.packages) 
-    library(raster); library(ggplot2); library(plyr); library(data.table); library(stringr); library(dplyr)
-    
-  # clause on type of analysis to be run
-    if (reflective == TRUE) {
-      
-      date.start = date.start 
-      date.end = date.end
-    } else {
-      
-      date.start = Sys.time()-60*60*24*7
-      date.end = Sys.time()
-    }
-    
-    date.interval <- interval(date.start, date.end, tzone = 'Pacific/Auckland')
+    list.of.packages <- c("raster","ggplot2","plyr","data.table","stringr","dplyr");
+    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])];
+    if(length(new.packages)) install.packages(new.packages); 
+    library(raster);library(ggplot2);library(plyr);library(data.table);library(stringr);library(dplyr);
+       
 
   # define selected variables
   # use `data.table` package to deal with large datasets
-    x <- as.data.table(x)
-    x[, date := ymd_hms(date, tz = 'Pacific/Auckland')]
-    x <- x[date %within% date.interval][, date := force_tz(date, tzone = 'Pacific/Auckland')]
-    x$proxy <- x[,..proxy]
-    x$obs <- x[,..obs]
+    x <- as.data.table(x);
+    x[, date := ymd_hms(date)];
+    
+     # clause on type of analysis to be run
+    if (reflective == TRUE){
+      
+      date.start = min(x$date, na.rm = T);
+      date.end = max(x$date, na.rm = T);
+      
+    } else {
+      
+      date.start = Sys.time()-60*60*24*7;
+      date.end = Sys.time();
+      date.start <- ymd_hms(date.start); date.end <- ymd_hms(date.end);
+      
+    };
+    
+    x <- x[date %within% interval(date.start, date.end)];
+    x$proxy <- x[,..proxy];
+    x$obs <- x[,..obs];
     
   # select the columns for day, groups, daily variance & n of sites
-    x <- x[, list(date, obs, proxy)]
+    x <- x[, list(date, obs, proxy)];
     
   # remove repeating and empty rows
-    x <- unique.data.frame(x)
-    x <- na.omit(x)
+    x <- unique.data.frame(x);
+    x <- na.omit(x);
 
   # generate rolling KS test results using function by site
-    if(length(x$obs) > window.length) {
-      ks.x <- rollingKStest(x, obs = 'obs', proxy = 'proxy', window = window.length)
+    if(length(x$obs) > window.length){
+      ks.x <- rollingKStest(x, obs = 'obs', proxy = 'proxy', window = window.length);
     } else {
-      ks.x <- cbind(x, p.value = rep(NA, length(x[[1]])), statistic = rep(NA, length(x[[1]])))
+      ks.x <- cbind(x, p.value = rep(NA, length(x[[1]])), statistic = rep(NA, length(x[[1]])));
     }
-    ks.x <- as.data.table(ks.x)
+    ks.x <- as.data.table(ks.x);
     
   # use theta and tau thresholds if present
-    if((!is.na(theta))) {
+    if(!is.na(theta)){
   
-		ks.x <- setDT(ks.x)[, warning := ifelse(p.value < theta, 1, 0)]
-		ks.x <- ks.x[, warning := ifelse(is.na(warning), 0, warning)]
+		ks.x <- setDT(ks.x)[, warning := ifelse(p.value < theta, 1, 0)];
+		ks.x <- ks.x[, warning := ifelse(is.na(warning), 0, warning)];
 		
-      if((!is.na(tau))) {
+      if(!is.na(tau)){
 
       # if else clause on whether the data are less than theta, becomes 1 if true, and 0 otherwise
       # this looks at running means and is for tau, the length of time for alarms
       
-        ks.x <- ks.x[, alarm := movingFun(warning, n = tau, type = 'to', fun = mean, na.rm = T)] 
-        ks.x <- ks.x[, alarm := ifelse(is.na(alarm), 0, alarm)]
+        ks.x <- ks.x[, alarm := movingFun(warning, n = tau, type = 'to', fun = mean, na.rm = T)];
+        ks.x <- ks.x[, alarm := ifelse(is.na(alarm), 0, alarm)];
    
       } else {
     
-        ks.x$alarm <- NA
-      } 
+        ks.x$alarm <- NA;
+      }; 
     }  else {
   
-      ks.x$warning <- NA
-      ks.x$alarm <- NA
-    }
+      ks.x$warning <- NA;
+      ks.x$alarm <- NA;
+      
+    };
 
   # gather variables of interest and return
-    ks.x <- ks.x[, list(date, test = 'ks test', statistic = p.value, warning, alarm)]
+    ks.x <- ks.x[, list(date, test = 'ks test', statistic = p.value, warning, alarm)];
 
-	ks.x
+	ks.x;
 	
-}
+};
   
